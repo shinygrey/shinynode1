aa5936a623b3f20099013cae658d9894cef2c692
Shiny Grey ltd <42606419+shinygrey@users.noreply.github.com>
2018-11-02 15:30:38 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var http = require('http');
 
-var server = http.createServer(function(request, response) {
-
+var server = http.createServer(function(request, response) {    
+    console.log(process.env.GREG_VAR);
     response.writeHead(200, {"Content-Type": "text/plain"});
     response.end("Hello Greg!");
 
