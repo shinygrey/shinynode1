a6eff126dc243f0e49be0e4ab8978a5c6e641680
Shiny Grey ltd <42606419+shinygrey@users.noreply.github.com>
2018-11-03 22:14:50 +0000

    Update index.js

--- index.js
+++ index.js
@@ -47,57 +47,46 @@ const httpOptions = {
 		'Authorization': 'OAuth oauth_consumer_key="'+process.env.TWITTER_CONSUMER_KEY+'", oauth_nonce="'+ Date.now() +'", oauth_signature="'+oauthSignature+'", oauth_signature_method="HMAC-SHA1", oauth_timestamp="'+Date.now()+'", oauth_token="'+process.env.TWITTER__ACCESS_TOKEN+'", oauth_version="1.0"'
 	}
 };
-/*
+
 try {
-
 	http.get('https://reqres.in/api/users/2', (res) => {
-	  const { statusCode } = res;
-	  const contentType = res.headers['content-type'];
+		const { statusCode } = res;
+		const contentType = res.headers['content-type'];
 
-	  let error;
-	  if (statusCode !== 200) {
-		error = new Error('Request Failed.\n' +
-						  `Status Code: ${statusCode}`);
-	  } else if (!/^application\/json/.test(contentType)) {
-		error = new Error('Invalid content-type.\n' +
-						  `Expected application/json but received ${contentType}`);
-	  }	  
-	  if (error) {
-		try {
-		browsermessage = if(typof error != string){browsermessage + "it's not a string "+typof error}else{browsermessage + error};
-		}catch(err){browsermessage =  browsermessage + "\ndoesn't work that way";}
-				
+		let error;
+		if (statusCode !== 200){
+			error = new Error('Request Failed.\n' + `Status Code: ${statusCode}`);
+		}else if(!/^application\/json/.test(contentType)){
+			error = new Error('Invalid content-type.\n' + `Expected application/json but received ${contentType}`);
+		}
+		if (error) {
+		console.error(error.message);
 		// consume response data to free up memory
 		res.resume();
 		return;
-	  }
+		}
 
-	  res.setEncoding('utf8');
+	  /*res.setEncoding('utf8');
 	  let rawData = '';
 	  res.on('data', (chunk) => { rawData += chunk; });
 	  res.on('end', () => {
 		try {
 		  const parsedData = JSON.parse(rawData);
-		  browsermessage =  browsermessage +"\n\n"+parsedData;
+		  console.log(parsedData);
 		} catch (e) {
-		  try {console.error(e.message)}catch(){};
+		  console.error(e.message);
 		}
-	  });
-	}).on('error', (e) => {
-	  console.error(`Got error: ${e.message}`);
-	});
-
-}catch(err){{browsermessage =  browsermessage + "\n no good: "+err;}
-
-*/
-
+	  });*/	  
+	  
+	})
+}catch(err){}
 
 
 var server = http.createServer(function(request, response) {
 var greg = process.env.GREG_VAR;
 response.writeHead(200, {"Content-Type": "text/plain"});
 response.end(
-"Hello Greg!  "+greg+" ... The type of the var is "+typeof greg+"\n"
+"Hello Greg!  "+greg+" ... \n"
 + browsermessage +  "\n"
 );
 });
