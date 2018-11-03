3f6492b4cc8a763f16bc7739c665caa4f338f9cc
Shiny Grey ltd <42606419+shinygrey@users.noreply.github.com>
2018-11-03 21:14:06 +0000

    Update index.js

--- index.js
+++ index.js
@@ -2,42 +2,38 @@ var http = require('http');
 var browsermessage
 const crypto = require('crypto');
 
+var urlUserTimeline = 'https://api.twitter.com/1.1/statuses/user_timeline.json';
+var twitterurl = encodeURIComponent(this.urlUserTimeline);
+var method = "GET";
+var oauthConsumerKey = process.env.TWITTER_CONSUMER_KEY;
+var oauthAccessToken = process.env.TWITTER__ACCESS_TOKEN;
+var oauthParams = encodeURIComponent(
+"oauth_consumer_key=" + 
+oauthConsumerKey + "&oauth_nonce=" + Date.now() + 
+"&oauth_signature_method=HMAC-SHA1&oauth_timestamp=" + Date.now() + "&oauth_token=" + oauthAccessToken
+);
+var oauthBaseString = method + "&" + twitterurl + "&" + oauthParams;
+var oauthSignatureKey = process.env.TWITTER_CONSUMER_SECRET + "&" + process.env.TWITTER__ACCESS_TOKEN_SECRET;
+
 try {
-
-	var urlUserTimeline = 'https://api.twitter.com/1.1/statuses/user_timeline.json';
-	var twitterurl = encodeURIComponent(this.urlUserTimeline);
-	var method = "GET";
-	var oauthConsumerKey = process.env.TWITTER_CONSUMER_KEY;
-	var oauthAccessToken = process.env.TWITTER__ACCESS_TOKEN;
-	var oauthParams = encodeURIComponent(
-	"oauth_consumer_key=" + 
-	oauthConsumerKey + "&oauth_nonce=" + Date.now() + 
-	"&oauth_signature_method=HMAC-SHA1&oauth_timestamp=" + Date.now() + "&oauth_token=" + oauthAccessToken
-	);
-
-	var oauthBaseString = method + "&" + twitterurl + "&" + oauthParams;
-
-	var oauthSignatureKey = process.env.TWITTER_CONSUMER_SECRET + "&" + process.env.TWITTER__ACCESS_TOKEN_SECRET;
-
-
-
-	try {
 	crypto.createHmac('sha1',oauthSignatureKey);
-	} catch (err) {
-	browsermessage = "problem "+err ;
-	}
-}catch(err){
-	browsermessage = "big problem "+err ;
+} catch (err){
+	browsermessage = "createHmac problem "+err ;
 }
 
 try {
-	hmac.update(Oauth.oauthBaseString);
-	var oauthSignature = hmac.digest('base64');
-	browsermessage = "worked";
+	hmac.update(oauthBaseString);
 }catch(err){
-browsermessage = "big problem "+err ;
+	browsermessage = browsermessage +" hmac.update problem "+err ;
 }
 
+try {
+	var oauthSignature = hmac.digest('base64');
+}catch(err){
+	browsermessage = browsermessage +" hmac.digest problem "+err ;
+}
+
+browsermessage = browsermessage +" worked";
 
 var server = http.createServer(function(request, response) {
 var greg = process.env.GREG_VAR;
