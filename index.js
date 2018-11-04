1c13150a022fc1228a95be546b56f6d9ea503114
Shiny Grey ltd <42606419+shinygrey@users.noreply.github.com>
2018-11-04 11:53:47 +0000

    Update index.js

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ const httpOptions = {
 	}
 };
 
-function getRequest(){
+(function getRequest(){
 	var requestmessage = "\nstart"
 	if(envProtocol == "https"){var protocol = https}else{var protocol = http};
 	protocol.get(envRequestUrl, (res) => {
@@ -75,7 +75,7 @@ function getRequest(){
 		});
 	})
 	browsermessage = browsermessage + requestmessage;
-}
+})()
 
 var server = http.createServer(function(request, response) {
 	response.writeHead(200, {"Content-Type": "text/plain"});
