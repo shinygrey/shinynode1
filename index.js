60f4879ecdbf07528011ee743d6bf5341b4da490
Shiny Grey ltd <42606419+shinygrey@users.noreply.github.com>
2018-11-07 18:53:20 +0000

    Update index.js

--- index.js
+++ index.js
@@ -2,13 +2,9 @@ const http = require("http");
 const fs = require("fs");
 const path = require("path");
 
-/*
 const {RestRequest} = require("../app/restrequest.js");
 
-RestRequest.getRequest()
-<p>${RestRequest.responseData}</p>
-
-*/
+RestRequest.getRequest();
 
 http.createServer(function(request, response){
 	response.writeHead(200, {"Content-Type": "text/html"});
@@ -17,23 +13,8 @@ http.createServer(function(request, response){
 		<body>
 			<h1>Hello!</h1>
 			<p>Object based request:</p>
-			
+			<p>${RestRequest.responseData}</p>
 		</body>
 		</html>
 	`).replace(/^\t\t/gm, ''));
 }).listen(process.env.PORT);
-
-
-/*
-var server = http.createServer(function(request, response) {
-	response.writeHead(200, {"Content-Type": "text/plain"});
-	response.end(
-		"Hello Greg!  "+envGreg+" ... \n"
-		+ browsermessage +  "\n"
-		+ atweet.test +  "\n"
-	);
-});
-	
-var port = process.env.PORT || 1337;
-server.listen(port);
-*/
