2156ee25b3d4b29247ed68cffe38c5f7fa5e3fa2
Shiny Grey ltd <42606419+shinygrey@users.noreply.github.com>
2018-11-07 18:29:36 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
 const http = require("http");
 const fs = require("fs");
 const path = require("path");
-/*
+
 const {RestRequest} = require("../app/restrequest.js");
-*/
-//RestRequest.getRequest();<p>${RestRequest.responseData}</p>
+
+RestRequest.getRequest();
 
 http.createServer(function(request, response){
-	response.writeHeader(200, {"Content-Type": "text/html"});
+	response.write(Header(200, {"Content-Type": "text/html"}));
 	console.log(request.url);
 	response.end((`
 		<html>
 		<body>
 			<h1>Hello!</h1>
 			<p>Object based request:</p>
-			
+			<p>${RestRequest.responseData}</p>
 		</body>
 		</html>
 	`).replace(/^\t\t/gm, ''));
