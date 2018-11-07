c87af7d046b989a9aac185ea716ad4ba426d516d
Shiny Grey ltd <42606419+shinygrey@users.noreply.github.com>
2018-11-07 15:47:37 +0000

    Update index.js

--- index.js
+++ index.js
@@ -3,69 +3,8 @@ const fs = require("fs");
 const path = require("path");
 
 const {RestRequest} = require("../app/restrequest.js");
-const {Twitter} = require("../app/twitter.js");
 
-var envProtocol = "http";
-//var envRequestUrl = "http://northwind.servicestack.net/query/customers.json?Ids=CHOPS,FRANK";
-
-var oauthForTwitter = new Twitter();
-
-//var northwind = new RestRequest("http://northwind.servicestack.net/query/customers.json?Ids=CHOPS");
-/*var regres = new RestRequest("https://reqres.in/api/users");*/
-
-
-function highOrderExample(){
-	function repeat(n, action) {
-	for (let i = 0; i < n; i++) {
-		action(i);
-	}
-}
-	repeat(2, function(i){
-		labels.push(`Unit ${i + 1}`)
-	});
-
-	repeat(2, i => {
-		labels.push(`Unit ${i + 1}`);
-	});
-}
-
-
-const twitterOptions = {
-	hostname: 'api.twitter.com',
-	port: 80,
-	path: '/1.1/statuses/user_timeline.json',
-	method: 'GET',
-	headers:{
-		'Accept': '*/*',
-		'Connection': 'close',
-		'User-Agent': 'OAuth gem v0.4.4',
-		'Content-Type': 'application/x-www-form-urlencoded',
-		'Authorization':`${oauthForTwitter.getAuth()}`,
-		'Content-Length': 76,
-		'Host': 'api.twitter.com'
-	}
-};
-
-var theTwitterResponse = ""; 
-
-function twitterRest(){
-	https.get('https://api.twitter.com/1.1/statuses/user_timeline.json', twitterOptions, (res) => {
-	const { statusCode } = res;
-	const contentType = res.headers['content-type'];
-	if (statusCode !== 200){
-		var error = new Error('Request Failed.\n' + `Status Code: ${statusCode}`);
-	}else if(!/^application\/json/.test(contentType)){
-		var error = new Error('Invalid content-type.\n' + `Expected application/json but received ${contentType}`);
-	}
-	if (statusCode !== 200){
-		theTwitterResponse += "\nerrors here:\n" + error.message + "\n";
-		res.resume();
-		return;
-	}
-	res.setEncoding('utf8');
-	res.on('data', (chunk) => { theTwitterResponse += chunk; });
-})};
-
+RestRequest.getRequest();
 
 fs.readFile('C:/Users/Greg/Documents/githubrepos/node-test2/index.html', function (err, html) {
 	if (err){
@@ -86,9 +25,8 @@ fs.readFile('C:/Users/Greg/Documents/githubrepos/node-test2/index.html', functio
 		<html>
 		<body>
 		<h1>Hello!</h1>
-		<p>Twitter:</p>
-		<p>${oauthForTwitter.getAuth()}</p>
-		<p>${theTwitterResponse}</p>
+		<p>Data:</p>
+		<p>${RestRequest.responseData}</p>
 		</body>
 		</html>
 		`
